2d10bc25b88753f9a68f912a9df38adedc14053f
Lairon Souza <laionconectado@gmail.com>
2022-05-17 00:09:47 -0300

    adcionando script em python

---
+++ script.py
@@ -0,0 +1,7 @@
+#teste de script em python, feito direto no terminal bash
+
+nome = "Lairon Souza"
+idade = 28
+texto = "Olá, me chamo nome, e estou estudando crypto, e tenho idade anos"
+
+print(texto)
